76b1100d4a367bed231120c5ccb044d5b296ff80
juanignacioalonso <zalbak87@gmail.com>
2025-02-11 21:15:56 -0300

    Se crea el componente list en dbz

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
 
+    MainPageComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
-    MainPageComponent,
+
   ],
   exports:[
-    MainPageComponent
+    MainPageComponent,
+
   ],
 })
 export class DbzModule { }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dbz-main-page',
-  templateUrl: './main-page.component.html'
+  templateUrl: './main-page.component.html',
+  standalone: false,
 })
 
 export class MainPageComponent {
