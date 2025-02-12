d844cb94e3b2555f9e508747afec2ab9670aa457
juanignacioalonso <zalbak87@gmail.com>
2025-02-11 21:32:11 -0300

    creacion del componente formulario dbz

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  standalone: false,
+
+  templateUrl: './add-character.component.html',
+  styleUrl: './add-character.component.css'
+})
+export class AddCharacterComponent {
+
+}
